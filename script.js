55bba037e8169d762ee1b551bf5217bc87e45152
Ryan Zegray <rzegray@uwo.ca>
2016-03-12 09:18:11 -0500

    Added stylings to main website

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
+//Javascript for the popup
 var MainController = function ($scope) {
-    console.log(JSON.parse(localStorage["filterArray"]));
+    //load the array of filters from local storage
     $scope.filters = JSON.parse(localStorage["filterArray"]);
+
+    console.log(document.body);
+    //method for adding filters to the filter arrays
     $scope.addFilter = function (name) {
+        //if the name inputted is empty
         if (name === undefined || name === null || name === '') {
             return;
         }
-        var filtersLength = $scope.filters.length;
         var newFilter = name.toLocaleLowerCase();
         document.getElementById('textbox').value = '';
+
+        //loop through the array to make sure you arent adding an already existing filter
+        var filtersLength = $scope.filters.length;
         var valid = true;
         for (var i = 0; i < filtersLength; i++) {
             if ($scope.filters[i].name === newFilter) {
@@ -24,6 +31,7 @@ var MainController = function ($scope) {
         localStorage["filterArray"] = JSON.stringify($scope.filters);
     }
 
+    //method for removing filters from the filter array
     $scope.removeFilter = function (index) {
         $scope.filters.splice(index, 1);
         localStorage["filterArray"] = JSON.stringify($scope.filters);

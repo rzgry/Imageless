2c722571c98a7bc2a5e894862afd5e2223bdcae9
Ryan Zegray <rzegray@uwo.ca>
2016-03-12 16:36:18 -0500

    updated and polished UI

--- script.js
+++ script.js
@@ -2,16 +2,28 @@
 var MainController = function ($scope) {
     //load the array of filters from local storage
     if (localStorage.getItem("filterArray") === null) {
-        var temp = [{name: 'nsfw'} , {name: 'salad'} , {name: 'monkey'} , { name: 'slimy'}]; 
+        var temp = [{
+            name: 'nsfw'
+        }, {
+            name: 'salad'
+        }, {
+            name: 'monkey'
+        }, {
+            name: 'slimy'
+        }];
         localStorage["filterArray"] = JSON.stringify(temp);
     }
     $scope.filters = JSON.parse(localStorage["filterArray"]);
+    var textbox = document.getElementById('textbox');
+    var warning = document.getElementById('warningmsg');
 
     console.log(document.body);
     //method for adding filters to the filter arrays
     $scope.addFilter = function (name) {
         //if the name inputted is empty
         if (name === undefined || name === null || name === '') {
+            textbox.style.borderColor = "red";
+            warning.textContent = 'Not a valid filter';
             return;
         }
         var newFilter = name.toLocaleLowerCase();
@@ -23,6 +35,8 @@ var MainController = function ($scope) {
         for (var i = 0; i < filtersLength; i++) {
             if ($scope.filters[i].name === newFilter) {
                 valid = false;
+                textbox.style.borderColor = "red";
+                warning.textContent = 'Filter exists';
             }
         }
 
@@ -30,6 +44,8 @@ var MainController = function ($scope) {
             $scope.filters.push({
                 name: newFilter
             });
+            textbox.style.borderColor = "#33cc33";
+            warning.textContent = '';
         }
 
         localStorage["filterArray"] = JSON.stringify($scope.filters);

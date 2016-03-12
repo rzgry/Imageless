4b48a9feb9885c2f27863b66c8d16996eb283554
Ryan Zegray <rzegray@uwo.ca>
2016-03-12 09:53:45 -0500

    Commenting changes

--- content.js
+++ content.js
@@ -1,1 +1,9 @@
-console.log(document.getElementsByTagName('html')[0].innerHTML);
+//This code is loaded for every website and checks for any images
+var images = document.getElementsByTagName('img');
+if (images.length <= 0) {
+  return;
+}
+var imageArray = [];
+for (var i = 0; i < images.length; i++) {
+    console.log(images[i].src);
+}
